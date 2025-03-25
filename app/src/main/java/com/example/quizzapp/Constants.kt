b4c1eed566d3df80ea3a9fb2c63d960a9e76a095
Aymen Hassini <hassiniaymen@yahoo.com>
2025-03-25 09:59:01 +0100

    correcting Constants.kt

--- app/src/main/java/com/example/quizzapp/Constants.kt
+++ app/src/main/java/com/example/quizzapp/Constants.kt
@@ -2,165 +2,62 @@ package com.example.quizzapp
 
 object Constants {
 
-    fun getQuestions(): ArrayList<Question> {
-        val questionsList = ArrayList<Question>()
-        val que0 = Question(
-            id = 1,
-            question = "What country does this flag belong to?",
-            image = R.drawable.ic_flag_of_tunisia,
-            optionOne = "Turkey",
-            optionTwo = "Tunis",
-            optionThree = "Egypt",
-            optionFour = "Germany",
-            correctAnswer = 2
+    var myQuestionsList: ArrayList<Question> = arrayListOf(
+        Question(
+            1,
+            R.drawable.ic_flag_of_algeria,
+            "Morocco", "Libya", "Nigeria", "Algeria", 4
+        ),
+        Question(
+            2,
+            R.drawable.ic_flag_of_argentina,
+            "Argentina", "Chile", "Uruguay", "Paraguay", 1
+        ),
+        Question(
+            3,
+            R.drawable.ic_flag_of_australia,
+            "New Zealand", "Fiji", "Indonesia", "Australia", 4
+        ),
+        Question(
+            4,
+            R.drawable.ic_flag_of_belgium,
+            "Netherlands", "France", "Belgium", "Luxembourg", 3
+        ),
+        Question(
+            5,
+            R.drawable.ic_flag_of_brazil,
+            "Colombia", "Brazil", "Argentina", "Venezuela", 2
+        ),
+        Question(
+            6,
+            R.drawable.ic_flag_of_denmark,
+            "Norway", "Finland", "Denmark", "Sweden", 3
+        ),
+        Question(
+            7,
+            R.drawable.ic_flag_of_fiji,
+            "Fiji", "Samoa", "Tonga", "Vanuatu", 1
+        ),
+        Question(
+            8,
+            R.drawable.ic_flag_of_germany,
+            "Austria", "Switzerland", "Poland", "Germany", 4
+        ),
+        Question(
+            9,
+            R.drawable.ic_flag_of_india,
+            "Pakistan", "Bangladesh", "Nepal", "India", 4
+        ),
+        Question(
+            10,
+            R.drawable.ic_flag_of_kuwait,
+            "Qatar", "Kuwait", "Bahrain", "UAE", 2
+        ),
+        Question(
+            11,
+            R.drawable.ic_flag_of_new_zealand,
+            "Australia", "Fiji", "New Zealand", "Papua New Guinea", 3
         )
-        questionsList.add(que0)
-
-        // TODO: ADD ALL QUESTIONS THAT WILL BE DISPLAYED
-
-        questionsList.add(
-            Question(
-                id = 2,
-                question = "What country does this flag belong to?",
-                image = R.drawable.ic_flag_of_algeria,
-                optionOne = "Morocco",
-                optionTwo = "Libya",
-                optionThree = "Nigeria",
-                optionFour = "Algeria",
-                correctAnswer = 4
-            )
-        )
-
-        questionsList.add(
-            Question(
-                id = 3,
-                question = "What country does this flag belong to?",
-                image = R.drawable.ic_flag_of_argentina,
-                optionOne = "Argentina",
-                optionTwo = "Chile",
-                optionThree = "Uruguay",
-                optionFour = "Paraguay",
-                correctAnswer = 1
-            )
-        )
-
-        questionsList.add(
-            Question(
-                id = 4,
-                question = "What country does this flag belong to?",
-                image = R.drawable.ic_flag_of_australia,
-                optionOne = "New Zealand",
-                optionTwo = "Fiji",
-                optionThree = "Indonesia",
-                optionFour = "Australia",
-                correctAnswer = 4
-            )
-        )
-
-        questionsList.add(
-            Question(
-                id = 5,
-                question = "What country does this flag belong to?",
-                image = R.drawable.ic_flag_of_belgium,
-                optionOne = "Netherlands",
-                optionTwo = "France",
-                optionThree = "Belgium",
-                optionFour = "Luxembourg",
-                correctAnswer = 3
-            )
-        )
-
-        questionsList.add(
-            Question(
-                id = 6,
-                question = "What country does this flag belong to?",
-                image = R.drawable.ic_flag_of_brazil,
-                optionOne = "Colombia",
-                optionTwo = "Brazil",
-                optionThree = "Argentina",
-                optionFour = "Venezuela",
-                correctAnswer = 2
-            )
-        )
-
-        questionsList.add(
-            Question(
-                id = 7,
-                question = "What country does this flag belong to?",
-                image = R.drawable.ic_flag_of_denmark,
-                optionOne = "Norway",
-                optionTwo = "Finland",
-                optionThree = "Denmark",
-                optionFour = "Sweden",
-                correctAnswer = 3
-            )
-        )
-
-        questionsList.add(
-            Question(
-                id = 8,
-                question = "What country does this flag belong to?",
-                image = R.drawable.ic_flag_of_fiji,
-                optionOne = "Fiji",
-                optionTwo = "Samoa",
-                optionThree = "Tonga",
-                optionFour = "Vanuatu",
-                correctAnswer = 1
-            )
-        )
-
-        questionsList.add(
-            Question(
-                id = 9,
-                question = "What country does this flag belong to?",
-                image = R.drawable.ic_flag_of_germany,
-                optionOne = "Austria",
-                optionTwo = "Switzerland",
-                optionThree = "Poland",
-                optionFour = "Germany",
-                correctAnswer = 4
-            )
-        )
-
-        questionsList.add(
-            Question(
-                id = 10,
-                question = "What country does this flag belong to?",
-                image = R.drawable.ic_flag_of_india,
-                optionOne = "Pakistan",
-                optionTwo = "Bangladesh",
-                optionThree = "Nepal",
-                optionFour = "India",
-                correctAnswer = 4
-            )
-        )
-
-        questionsList.add(
-            Question(
-                id = 11,
-                question = "What country does this flag belong to?",
-                image = R.drawable.ic_flag_of_kuwait,
-                optionOne = "Qatar",
-                optionTwo = "Kuwait",
-                optionThree = "Bahrain",
-                optionFour = "UAE",
-                correctAnswer = 2
-            )
-        )
-
-        questionsList.add(
-            Question(
-                id = 12,
-                question = "What country does this flag belong to?",
-                image = R.drawable.ic_flag_of_new_zealand,
-                optionOne = "Australia",
-                optionTwo = "Fiji",
-                optionThree = "New Zealand",
-                optionFour = "Papua New Guinea",
-                correctAnswer = 3
-            )
-        )
-
-        return questionsList
-    }
+    )
 }
+

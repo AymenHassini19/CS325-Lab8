597320840233a636f9714e1a4ba5e062f91ca775
Aymen Hassini <hassiniaymen@yahoo.com>
2025-03-25 09:27:26 +0100

    linking variables MainActivity

--- app/src/main/java/com/example/quizzapp/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.quizzapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +13,14 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private var tvQuestion:TextView? = null
+    private var iv1:ImageView? = null
+    private var pbQuizProgress:ProgressBar? = null
+    private var tvQuizProgress:TextView? = null
+    private var tvOptionOne:TextView? = null
+    private var tvOptionTwo:TextView? = null
+    private var tvOptionThree:TextView? = null
+    private var tvOptionFour:TextView? = null
+    private var btnSubmit:Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +33,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         tvQuestion = findViewById(R.id.tv_question)
+        iv1 = findViewById(R.id.iv1)
+        pbQuizProgress = findViewById(R.id.pb_quiz_progress)
+        tvQuizProgress = findViewById(R.id.tv_quiz_progress)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+        btnSubmit = findViewById(R.id.btn_submit)
+        
 
     }
 }
